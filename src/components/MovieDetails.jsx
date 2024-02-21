8c6488489a454e399995543832a81abbc1ef56ae
Carissa Ward <cward52@my.bcit.ca>
2024-02-21 12:11:34 -0800

    message added to details page, needs to be styled still

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,27 +1,53 @@
-// Import PropTypes for type-checking
+// Importing PropTypes for type-checking
 import PropTypes from 'prop-types';
 
-// Import dateFormat for date formatting
+// Importing dateFormat for date formatting
 import dateFormat from 'dateformat';
 
-// Import utility functions
+// Importing utility functions
 import { useMovieStatus, generateStars, getPosterPath } from '../utilities/useMovieStatus';
 
-// Import the SCSS file for styling
-import "../styles/MovieDetails.scss";
+// Importing useSelector and useDispatch hooks from React Redux
+import { useSelector, useDispatch } from 'react-redux';
 
+// Importing action creators for notification handling
+import { setMessage, clearMessage } from '../features/notification/notificationSlice';
+
+// Importing the SCSS file for styling
+import "../styles/MovieDetails.scss";
 
 // MovieDetails component definition
 function MovieDetails({ movie }) {
     // Destructuring movie object
-    const { title, overview, poster_path, release_date, vote_average } = movie;
+    const { id, title, overview, poster_path, release_date, vote_average } = movie;
 
     // Getting movie status using custom hook
-    const { isFavorite, isInWatchList, toggleFavorite, toggleWatchList } = useMovieStatus(movie.id);
+    const { isFavorite, isInWatchList, toggleFavorite, toggleWatchList } = useMovieStatus(id);
+
+    const dispatch = useDispatch(); // useDispatch hook for dispatching actions
+
+    const message = useSelector(state => state.notification.message); // Getting notification message from Redux store
+
+    // Function to handle adding or removing a movie from favorites/watchlist
+    const handleListAction = (title, isAdding, listName) => {
+        if (isAdding) {
+            dispatch(setMessage(`'${title}' added to ${listName}`)); // Dispatching message when adding a movie
+        } else {
+            dispatch(setMessage(`'${title}' removed from ${listName}`)); // Dispatching message when removing a movie
+        }
+        setTimeout(() => {
+            dispatch(clearMessage()); // Clear the message after 3 seconds
+        }, 3000);
+    };
 
     return (
         <div id="details-wrapper"> {/* Container for movie details */}
-            <h3>Details for <i>{title}</i></h3> {/* Heading for movie details */}
+            <div id="title-message">
+                <h3>Details for <i>{title}</i></h3> {/* Heading for movie details */}
+                <div id="message">
+                    {message && <span>{message}</span>}
+                </div>
+            </div>
             <div className="line"></div> {/* Horizontal line separator*/}
             <div id="movie-details-wrapper"> {/* Container for movie details */}
                 <div>
@@ -29,11 +55,11 @@ function MovieDetails({ movie }) {
                 </div>
                 <div id="movie-details-data"> {/* Container for movie data */}
                     <div className="favorite-watchlist"> {/* Container for favorite and watchlist buttons */}
-                        <button onClick={toggleFavorite} title={isFavorite ? 'Remove from Favorites' : 'Add to Favorites'}>
+                        <button onClick={() => { toggleFavorite(); handleListAction(title, !isFavorite, 'Favorites'); }} title={isFavorite ? 'Remove from Favorites' : 'Add to Favorites'}>
                             <p className='favorite-label'>{isFavorite ? 'Remove from Favorites' : 'Add to Favorites'}</p>
                             <div className={`heart ${isFavorite ? 'favorite' : ''}`}></div> {/* Heart icon for favorite */}
                         </button>
-                        <button onClick={toggleWatchList} title={isInWatchList ? 'Remove from Watch List' : 'Add to Watch List'} className="plus-sign-button">
+                        <button onClick={() => { toggleWatchList(); handleListAction(title, !isInWatchList, 'Watch List'); }} title={isInWatchList ? 'Remove from Watch List' : 'Add to Watch List'} className="plus-sign-button">
                             <p className="watch-list-label">{isInWatchList ? 'Remove from Watch List' : 'Add to Watch List'}</p>
                             <div className={`plus-sign ${isInWatchList ? 'watchlist' : ''}`}></div> {/* Plus sign icon for watchlist */}
                         </button>
