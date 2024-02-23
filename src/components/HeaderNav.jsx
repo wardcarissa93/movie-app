d9617e76694ba45a727fee56253eda857adb4e29
Carissa Ward <cward52@my.bcit.ca>
2024-02-22 23:40:30 -0800

    several major styling changes

--- src/components/HeaderNav.jsx
+++ src/components/HeaderNav.jsx
@@ -66,7 +66,7 @@ const HeaderNav = () => {
 
       {/* Site logo */}
       <Link className={`logo ${isMenuOpen ? "hide" : ""}`} to={'/'} title="Return to Home">
-        <img src="images/logo.svg" alt="site-logo"></img>
+        <img src="../../images/logo-light.svg" alt="site-logo"></img>
       </Link>
 
       {/* Main navigation */}
