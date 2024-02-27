6ad2b885855722398bc006abebf7588a37d45a96
Carissa Ward <cward52@my.bcit.ca>
2024-02-27 10:02:02 -0800

    adjusted styling on pagewatchlist

--- src/utilities/useMovieStatus.jsx
+++ src/utilities/useMovieStatus.jsx
@@ -65,6 +65,6 @@ export const generateStars = (voteAverage) => {
 export const getPosterPath = (posterPath) => {
     return posterPath
       ? `https://image.tmdb.org/t/p/w500${posterPath}`
-      : '../../public/images/image-not-found.png'; // Fallback image path
+      : 'images/image-not-found.png'; // Fallback image path
   };
   
